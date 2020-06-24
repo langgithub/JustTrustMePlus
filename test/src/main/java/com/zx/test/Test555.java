92dd4e50bd7f5e42d087c55a7d7be0476c9db709
袁浪 <yuanlang@bigsfintech.com>
2020-06-24 14:19:05 +0800

    remove

--- test/src/main/java/com/zx/test/Test555.java
+++
@@ -1,9 +0,0 @@
-package com.zx.test;
-
-/**
- * Created by Lyh on
- * 2019/8/23
- */
-public class Test555 {
-
-}
--- test/src/main/java/com/zx/test/test/Hello/tttttttttttttttt.java
+++
@@ -1,9 +0,0 @@
-package com.zx.test.test.Hello;
-
-/**
- * Created by Lyh on
- * 2019/8/23
- */
-public interface tttttttttttttttt {
-    public void asfasdf();
-}
